33ebb87c3082ce8a9adc244d926ad30d1242fd51
rupert-ong <rupert.ong@gmail.com>
2018-05-19 15:50:01 -0400

    Add redux Store, Reducer and Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
 
-module.hot.accept();
+module.hot.accept(); 
