ef9dc0796fda0eb280cd76fc1c91a7222f3cef70
rupert-ong <rupert.ong@gmail.com>
2018-05-19 15:17:16 -0400

    Add state and event handlers to SearchBar container

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  state = {
+    term: ''
+  }
+
+  onInputChange = e => {
+    this.setState({
+      term: e.target.value
+    });
+  }
+
+  onFormSubmit = e => {
+    e.preventDefault();
+  }
+
   render() {
+    const { term } = this.state;
     return (
-      <form className="input-group mb-3">
-        <input type="text" className="form-control" placeholder="Enter a City Name" />
+      <form className="input-group mb-3" onSubmit={this.onFormSubmit}>
+        <input
+          type="text"
+          value={term}
+          className="form-control"
+          placeholder="Enter a City Name"
+          onChange={this.onInputChange} />
         <div className="input-group-append">
           <button type="submit" className="btn btn-outline-secondary">Submit</button>
         </div>
