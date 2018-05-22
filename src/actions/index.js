ff1b505b0829d7ebca6e5c465480cdb354d8275b
rupert-ong <rupert.ong@gmail.com>
2018-05-21 22:04:57 -0400

    Create FetchWeather action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 import { API_KEY } from '../env';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export const fetchWeather = (city) => {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
