033cdd858396610bed640278019f228d522bdba4
rupert-ong <rupert.ong@gmail.com>
2018-05-30 22:23:54 -0400

    Add simple error handling in Weather reducer

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,6 +1,9 @@
 import { FETCH_WEATHER } from '../actions';
 
 export default (state = [], action) => {
+  if(action.error) {
+    return state;
+  }
   switch(action.type){
     case FETCH_WEATHER:
       return [ action.payload.data, ...state ];
